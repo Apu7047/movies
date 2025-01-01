9ec4a1c327507122c7ad30c6fb95a695aa1fbb78
Apu7047 <apurbamura6@gmail.com>
2025-01-01 15:39:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -26,7 +26,7 @@ CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHAN
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Apu07:Apurba@7047@cluster0.61lzt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Apu07")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Clusters")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
